f8b9762c8b4b1c8a240eacf2acaf0b295bc9a2c6
Paul Pogonyshev <pogonyshev@gmail.com>
2020-01-15 22:56:17 +0100

    Fix copyright notices to include 2020.

--- eldev-util.el
+++ eldev-util.el
@@ -1,6 +1,6 @@
 ;;; eldev.el --- Elisp Development Tool  -*- lexical-binding: t -*-
 
-;;; Copyright (C) 2019 Paul Pogonyshev
+;;; Copyright (C) 2019-2020 Paul Pogonyshev
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
