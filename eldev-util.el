emacs-eldev/eldev

commit 971d89898a41443cd849350ff81ea50b0ae02b6e

Add variable `eldev-project-main-file' for projects where `package-dir-info' could pick a wrong file.

eldev-util.el
@@ -699,6 +699,14 @@ bound to the exit code of the process."
 (defvar eldev-project-dir nil
   "Directory of the project being built.")
 
+(defvar eldev-project-main-file nil
+  "Name of the file with project headers.
+If there is only one such file in the root directory, this can be
+left to nil.  In this case `package-dir-info' will automatically
+find the file.
+
+Since 0.2.")
+
 (defvar eldev--package-descriptors nil
   "Cache for `eldev-package-descriptor'.")
 
@@ -745,10 +753,14 @@ return the descriptor of the project being built."
   (let ((descriptor (unless skip-cache
                       (cdr (assoc project-dir eldev--package-descriptors)))))
     (unless descriptor
-      (setf descriptor (with-temp-buffer
-                         (setf default-directory project-dir)
-                         (dired-mode)
-                         (eldev--package-dir-info)))
+      (let ((eldev-project-main-file (eldev--cross-project-internal-eval project-dir 'eldev-project-main-file)))
+        (setf descriptor (with-temp-buffer
+                           (if eldev-project-main-file
+                               (progn (insert-file-contents (expand-file-name eldev-project-main-file project-dir))
+                                      (package-buffer-info))
+                             (let ((default-directory project-dir))
+                               (dired-mode)
+                               (eldev--package-dir-info))))))
       (unless skip-cache
         (push (cons project-dir descriptor) eldev--package-descriptors)))
     descriptor))

eldev.el
@@ -2759,6 +2759,21 @@ obligations."
                                       (eldev-listify feature-names)))))
 
 
+(defun eldev--cross-project-internal-eval (project-dir form)
+  (if (string= (file-name-as-directory project-dir) (file-name-as-directory eldev-project-dir))
+      (eval form t)
+    (eldev-trace "Starting a child Eldev process to evaluate form `%S' in directory `%s'" form project-dir)
+    ;; FIXME: Are we sure `--quiet' is enough to silence stderr?  Maybe redirect it instead?
+    (let ((default-directory project-dir))
+      (eldev-call-process (eldev-shell-command) `("--quiet" "exec" "--dont-load" ,(prin1-to-string `(prin1 ,form)))
+        (unless (= exit-code 0)
+          (eldev-warn "Output of the child Eldev process:\n%s" (buffer-string))
+          (signal 'eldev-error `("Failed to evaluate Eldev expression in directory `%s'" ,project-dir)))
+        (let ((result (read (current-buffer))))
+          (eldev-trace "Evaluated to `%S'" result)
+          result)))))
+
+
 
 ;; eldev emacs
 

commit 01c74650a647cb1ea14d317d960b3be3e5fd3d0b

Fix a byte-compilation warning caused by commit 971d898.

eldev-util.el
@@ -744,6 +744,8 @@ Since 0.2.")
             (error "No .el files with package headers in `%s'" default-directory))
           info)))))
 
+(declare-function eldev--cross-project-internal-eval "eldev" (project-dir form))
+
 (defun eldev-package-descriptor (&optional project-dir skip-cache)
   "Return descriptor of the package in PROJECT-DIR.
 If PROJECT-DIR is not specified, use `eldev-project-dir', i.e.