56e3591859ef297fad52c52a574dd857d33085bd
Paul Pogonyshev <pogonyshev@gmail.com>
2020-03-13 00:50:50 +0100

    Likewise, make `upgrade-self' command work even if invoked from a random non-project directory.

--- test/upgrade-self.el
+++ test/upgrade-self.el
@@ -1,22 +1,25 @@
 (require 'test/common)
 
 
+(defmacro eldev--test-do-upgrade-self->999.9 (&optional test-project)
+  `(let* ((eldev--test-project (or ,test-project eldev--test-project)))
+     (eldev--test-create-eldev-archive "eldev-archive-1")
+     (eldev--test-create-eldev-archive "eldev-archive-2" "999.9")
+     (let ((eldev--test-eldev-local (concat ":pa:" (eldev--test-tmp-subdir "eldev-archive-1")))
+           (eldev--test-eldev-dir   (eldev--test-tmp-subdir "upgrade-self-root")))
+       (ignore-errors (delete-directory eldev--test-eldev-dir t))
+       (eldev--test-run nil ("version")
+         (should (string= stdout (format "eldev %s\n" (eldev-message-version (eldev-find-package-descriptor 'eldev)))))
+         (should (= exit-code 0)))
+       (eldev--test-run nil ("--setup" (prin1-to-string `(setf eldev--upgrade-self-from-forced-pa ,(eldev--test-tmp-subdir "eldev-archive-2"))) "upgrade-self")
+         (should (string= stdout "Upgraded or installed 1 package\n"))
+         (should (= exit-code 0)))
+       (eldev--test-run nil ("version")
+         (should (string= stdout "eldev 999.9\n"))
+         (should (= exit-code 0))))))
+
 (ert-deftest eldev-upgrade-self-1 ()
-  (eldev--test-create-eldev-archive "eldev-archive-1")
-  (eldev--test-create-eldev-archive "eldev-archive-2" "999.9")
-  (let ((eldev--test-project     "trivial-project")
-        (eldev--test-eldev-local (concat ":pa:" (eldev--test-tmp-subdir "eldev-archive-1")))
-        (eldev--test-eldev-dir   (eldev--test-tmp-subdir "upgrade-self-root")))
-    (ignore-errors (delete-directory eldev--test-eldev-dir t))
-    (eldev--test-run nil ("version")
-      (should (string= stdout (format "eldev %s\n" (eldev-message-version (eldev-find-package-descriptor 'eldev)))))
-      (should (= exit-code 0)))
-    (eldev--test-run nil ("--setup" (prin1-to-string `(setf eldev--upgrade-self-from-forced-pa ,(eldev--test-tmp-subdir "eldev-archive-2"))) "upgrade-self")
-      (should (string= stdout "Upgraded or installed 1 package\n"))
-      (should (= exit-code 0)))
-    (eldev--test-run nil ("version")
-      (should (string= stdout "eldev 999.9\n"))
-      (should (= exit-code 0)))))
+  (eldev--test-do-upgrade-self->999.9 "trivial-project"))
 
 ;; Trying to upgrade from the archive we have bootstrapped.  Nothing to do.
 (ert-deftest eldev-upgrade-self-2 ()
@@ -34,6 +37,10 @@
     (eldev--test-run nil ("version")
       (should (string= stdout (format "eldev %s\n" (eldev-message-version (eldev-find-package-descriptor 'eldev)))))
       (should (= exit-code 0)))))
+
+;; Upgrading _self_ must succeed even from a non-project directory.
+(ert-deftest eldev-upgrade-self-3 ()
+  (eldev--test-do-upgrade-self->999.9 "empty-project"))
 
 
 (ert-deftest eldev-upgrade-self-dry-run-1 ()
