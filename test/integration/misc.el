c1455abea653ac26dc9b718365e90216363119a5
Paul Pogonyshev <pogonyshev@gmail.com>
2020-05-09 16:37:02 +0200

    Add a regression test for issue #10.

--- test/integration/misc.el
+++ test/integration/misc.el
@@ -1,6 +1,15 @@
 (require 'test/common)
 
 
+;; https://github.com/doublep/eldev/issues/10
+;; Command `upgrade-self' would fail.
+(ert-deftest eldev-issue-10 ()
+  (eldev--test-run "empty-project" ("upgrade-self" "--dry-run")
+    (should (= exit-code 0))))
+
+;; https://github.com/doublep/eldev/issues/12
+;; Autoloading Buttercup and using `melpa-stable' package archive in
+;; `Eldev' at the same time would fail.
 (ert-deftest eldev-issue-12 ()
   (eldev--test-without-files "issue-12-project" "mypkg.elc"
     (eldev--test-delete-cache)
