f345065592b4bc9a5173b44e12a74400335d4e49
Paul Pogonyshev <pogonyshev@gmail.com>
2020-05-10 17:21:58 +0200

    Release Eldev 0.3.2 of the new feature.

--- eldev.el
+++ eldev.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    0.3.1
+;; Version:    0.3.2
 ;; Keywords:   maint, tools
 ;; Homepage:   https://github.com/doublep/eldev
 ;; Package-Requires: ((emacs "24.4"))
