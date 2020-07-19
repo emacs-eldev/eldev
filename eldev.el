d4e227742495e8160d0f8aa4cb81e0e9b927bd0a
Paul Pogonyshev <pogonyshev@gmail.com>
2020-07-19 12:52:47 +0200

    Post-release version bump.

--- eldev.el
+++ eldev.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    0.6.1
+;; Version:    0.6.2snapshot
 ;; Keywords:   maint, tools
 ;; Homepage:   https://github.com/doublep/eldev
 ;; Package-Requires: ((emacs "24.4"))
