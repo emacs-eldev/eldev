5c115e966349aa03155f3062bd8a30ec32d95f26
Paul Pogonyshev <pogonyshev@gmail.com>
2020-08-30 18:30:55 +0200

    Fix a test broken on earlier Emacs versions by commit 7887ea3.

--- test/info.el
+++ test/info.el
@@ -8,7 +8,10 @@
 
 (ert-deftest eldev-info-missing-dependency-1 ()
   (eldev--test-run "missing-dependency-a" ("info")
-    (should (string= stdout "missing-dependency-a 1.0\n\nComments to make linters happy.\n"))
+    ;; Different Emacs versions result in different output, but I think it's not up to
+    ;; Eldev to change it, so we accept both.
+    (should (or (string= stdout "missing-dependency-a 1.0\n\nTest project with an unavailable dependency\n")
+                (string= stdout "missing-dependency-a 1.0\n\nComments to make linters happy.\n")))
     (should (= exit-code 0))))
 
 
