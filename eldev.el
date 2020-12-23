emacs-eldev/eldev

commit d4a116c2dc1e3af526c33bf419968f6dfad163a3

Also test `init' command for Subversion packages.

eldev.el
@@ -4918,8 +4918,10 @@ will fail if the project already has file named `Eldev'."
 
 ;; Using `autoload' function directly instead of ;;;###autoload cookies since the latter
 ;; won't work with ELDEV_LOCAL.
-(autoload 'eldev-active-plugins "eldev-plugins")
-(autoload 'eldev-use-plugin "eldev-plugins")
+(dolist (autoloads '(("eldev-plugins" eldev-active-plugins eldev-use-plugin)
+                     ("eldev-vc"      eldev-vc-root-dir eldev-vc-executable eldev-vc-full-name eldev-with-vc eldev-vc-detect)))
+  (dolist (function (cdr autoloads))
+    (autoload function (car autoloads))))
 
 
 (provide 'eldev)

eldev-util.el
@@ -763,6 +763,11 @@ through `executable-find' if possible.  Since Eldev 0.8.")
 Can be set explicitly or left to t, in which case it is located
 through `executable-find' if possible.  Since Eldev 0.8.")
 
+(defvar eldev-svnadmin-executable t
+  "Subversion administrator executable (svnadmin).
+Can be set explicitly or left to t, in which case it is located
+through `executable-find' if possible.  Since Eldev 0.8.")
+
 
 (defmacro eldev-find-executable (cache-var not-required finder-form error-message &rest error-arguments)
   "Find and executable using FINDER-FORM.
@@ -837,6 +842,13 @@ See also variable `eldev-svn-executable'."
   (eldev-find-executable eldev-svn-executable not-required
     (executable-find "svn")
     "Subversion is not installed (cannot find `svn' executable)"))
+
+(defun eldev-svnadmin-executable (&optional not-required)
+  "Find `svnadmin' executable.
+See also variable `eldev-svnadmin-executable'."
+  (eldev-find-executable eldev-svnadmin-executable not-required
+    (executable-find "svnadmin")
+    "Subversion is not installed (cannot find `svnadmin' executable)"))
 
 (defvar vc-svn-program)
 (with-eval-after-load 'vc-svn

eldev-vc.el
@@ -210,32 +210,8 @@ Try evaluating `(package-buffer-info)' in a buffer with the file")
         (if add-to-ignore
             (let (failed)
               (eldev-verbose "Adding %s to `%s'" (eldev-message-enumerate "file" add-to-ignore) .ignore)
-              (with-temp-buffer
-                (if (eq vc-backend 'SVN)
-                    ;; This can return non-null status if there is no such property yet.
-                    (eldev-call-process (eldev-svn-executable) '("propget" "svn:ignore" ".")
-                      :destination '(t nil))
-                  (condition-case nil
-                      (insert-file-contents .ignore t)
-                    ;; Pre-26 Emacsen don't know about `file-missing', so catch broadly.
-                    (file-error)))
-                (goto-char (point-max))
-                (unless (eolp)
-                  (insert "\n"))
-                (when (looking-back (rx nonl "\n") nil)
-                  (insert "\n"))
-                (unless (eq vc-backend 'SVN)
-                  (insert (eldev-format-message "# Added automatically by `eldev init'.\n")))
-                (dolist (file add-to-ignore)
-                  (insert (eldev-pcase-exhaustive vc-backend
-                            (`Git (format "/%s"  file))
-                            (`Hg  (format "^%s$" (regexp-quote file)))
-                            (`SVN file))
-                          "\n"))
-                (if (eq vc-backend 'SVN)
-                    (setf failed (/= (eldev-call-process (eldev-svn-executable) `("propset" "svn:ignore" ,(buffer-string) ".")) 0))
-                  (write-region nil nil .ignore nil 'quiet)))
-              (unless dont-check-if-ignored
+              (setf failed (eldev--vc-ignore-standard-files vc-backend files-to-ignore))
+              (unless (or failed dont-check-if-ignored)
                 (dolist (file files-to-ignore)
                   (when (/= (eldev-call-process (eldev-git-executable) `("check-ignore" "--quiet" ,file)) 0)
                     (eldev-warn "Failed to convince Git to ignore file `%s'" file)
@@ -262,6 +238,42 @@ Try evaluating `(package-buffer-info)' in a buffer with the file")
                                                   (setf before t
                                                         a      nil)))
                                               before)))))))))
+
+(defun eldev--vc-ignore-file (backend)
+  (eldev-pcase-exhaustive backend
+    (`Git ".gitignore")
+    (`Hg  ".hgignore")
+    ;; Not really a file, but close enough.
+    (`SVN "svn:ignore")))
+
+(defun eldev--vc-ignore-standard-files (backend &optional files)
+  (with-temp-buffer
+    (let ((.ignore (eldev--vc-ignore-file backend)))
+      (if (eq backend 'SVN)
+          ;; This can return non-null status if there is no such property yet.
+          (eldev-call-process (eldev-svn-executable) '("propget" "svn:ignore" ".")
+            :destination '(t nil))
+        (condition-case nil
+            (insert-file-contents .ignore t)
+          ;; Pre-26 Emacsen don't know about `file-missing', so catch broadly.
+          (file-error)))
+      (goto-char (point-max))
+      (unless (eolp)
+        (insert "\n"))
+      (when (looking-back (rx nonl "\n") nil)
+        (insert "\n"))
+      (unless (eq backend 'SVN)
+        (insert (eldev-format-message "# Added automatically by `eldev init'.\n")))
+      (dolist (file (or files `(,eldev-cache-dir ,eldev-local-file)))
+        (insert (eldev-pcase-exhaustive backend
+                  (`Git (format "/%s"  file))
+                  (`Hg  (format "^%s$" (regexp-quote file)))
+                  (`SVN file))
+                "\n"))
+      (if (eq backend 'SVN)
+          (/= (eldev-call-process (eldev-svn-executable) `("propset" "svn:ignore" ,(buffer-string) ".")) 0)
+        (write-region nil nil .ignore nil 'quiet)
+        nil))))
 
 
 (provide 'eldev-vc)

test/common.el
@@ -120,36 +120,60 @@ beginning.  Exit code of the process is bound as EXIT-CODE."
 
 (defmacro eldev--test-with-temp-copy (test-project vc-backend &rest body)
   (declare (indent 2) (debug (stringp sexp body)))
-  `(let* ((vc-backend          ,vc-backend)
-          (eldev--test-project (eldev--test-make-temp-copy ,test-project vc-backend)))
-     (unless eldev--test-project
-       (ert-skip (eldev-format-message "%s couldn't be found" vc-backend)))
-     ,@body))
+  (let ((ignores :std))
+    (while (keywordp (car body))
+      (eldev-pcase-exhaustive (pop body)
+        (:ignores (setf ignores (pop body)))))
+    `(let* ((vc-backend          ,vc-backend)
+            (eldev--test-project (eldev--test-make-temp-copy ,test-project vc-backend ,ignores)))
+       (unless eldev--test-project
+         (ert-skip (eldev-format-message "%s couldn't be found" vc-backend)))
+       (prog1 (progn ,@body)
+         ;; Delete the copy if there are no errors.
+         (ignore-errors (delete-directory eldev--test-project t))))))
 
-(defun eldev--test-make-temp-copy (test-project vc-backend)
-  (unless (memq vc-backend '(nil Git Hg))
+(defun eldev--test-make-temp-copy (test-project vc-backend ignores)
+  (unless (memq vc-backend '(nil Git Hg SVN))
     (error "Cannot create temporary VC copy for backend `%s'" vc-backend))
-  (let ((executable (if vc-backend (eldev-vc-executable vc-backend t) t)))
-    (when executable
-      (let ((project-dir (eldev--test-project-dir test-project))
-            (copy-dir    (file-name-as-directory (make-temp-file "eldev-" t "-test"))))
-        ;; To avoid copying generated files.
-        (eldev--test-run test-project ("clean" "everything")
-          (should (= exit-code 0)))
-        (copy-directory project-dir copy-dir t nil t)
-        (when vc-backend
-          (let ((default-directory copy-dir))
-            (eldev-call-process executable `("init") :die-on-error t)
-            (eldev-pcase-exhaustive vc-backend
-              (`Git (eldev-call-process executable `("config" "user.name"  "Eldev")             :die-on-error t)
-                    (eldev-call-process executable `("config" "user.email" "eldev@example.com") :die-on-error t))
-              ;; Mercurial doesn't appear to provide commands for this.
-              (`Hg  (with-temp-file ".hg/hgrc"
-                      (erase-buffer)
-                      (insert "[ui]\nusername = Eldev <eldev@example.com>\n"))))
-            (eldev-call-process executable `("add" ".")                   :die-on-error t)
-            (eldev-call-process executable `("commit" "--message" "Copy") :die-on-error t)))
-        copy-dir))))
+  (let* ((svnadmin   (when (eq vc-backend 'SVN) (eldev-svnadmin-executable t)))
+         (executable (if vc-backend
+                         (when (or (not (eq vc-backend 'SVN)) svnadmin)
+                           (eldev-vc-executable vc-backend t))
+                       t)))
+    (unless executable
+      (ert-skip (eldev-format-message "%s doesn't appear to be installed" (eldev-vc-full-name vc-backend))))
+    (let ((project-dir    (eldev--test-project-dir test-project))
+          (copy-dir       (file-name-as-directory (make-temp-file "eldev-" t (if vc-backend (format "-test-%s" (downcase (symbol-name vc-backend))) "-test"))))
+          (repository-dir (when svnadmin
+                            (file-name-as-directory (make-temp-file "eldev-" t "-test-svnrepo")))))
+      ;; To avoid copying generated files.
+      (eldev--test-run test-project ("clean" "everything")
+        (should (= exit-code 0)))
+      (when svnadmin
+        (eldev-call-process svnadmin   `("create" ,repository-dir) :die-on-error t)
+        (eldev-call-process executable `("mkdir" ,(format "file://%s/trunk" repository-dir) ,(format "file://%s/branches" repository-dir) "--message" "Init") :die-on-error t)
+        (eldev-call-process executable `("checkout" ,(format "file://%s/trunk" repository-dir) ,copy-dir) :die-on-error t))
+      (copy-directory project-dir copy-dir t nil t)
+      (when vc-backend
+        (let ((default-directory copy-dir))
+          (unless svnadmin
+            (eldev-call-process executable `("init") :die-on-error t))
+          (when (eq ignores :std)
+            (eldev--vc-ignore-standard-files vc-backend))
+          (pcase vc-backend
+            (`Git (eldev-call-process executable `("config" "user.name"  "Eldev")             :die-on-error t)
+                  (eldev-call-process executable `("config" "user.email" "eldev@example.com") :die-on-error t))
+            ;; Mercurial doesn't appear to provide commands for this.
+            (`Hg  (with-temp-file ".hg/hgrc"
+                    (erase-buffer)
+                    (insert "[ui]\nusername = Eldev <eldev@example.com>\n"))))
+          ;; Subversion doesn't allow 'add .'.
+          (let ((all-files (directory-files copy-dir)))
+            (dolist (special-file '("." ".." ".git" ".hg" ".svn"))
+              (setf all-files (remove special-file all-files)))
+            (eldev-call-process executable `("add" ,@all-files) :die-on-error t))
+          (eldev-call-process executable `("commit" "--message" "Copy") :die-on-error t)))
+      copy-dir)))
 
 
 (defun eldev--test-file-contents (test-project file &optional not-required)
@@ -164,7 +188,7 @@ beginning.  Exit code of the process is bound as EXIT-CODE."
   "Execute in a buffer with a FILE from TEST-PROJECT and write results back."
   (declare (indent 2) (debug (stringp sexp body)))
   `(with-temp-buffer
-     (insert-file-contents (expand-file-name ,file (eldev--test-project-dir ,test-project)) t)
+     (ignore-errors (insert-file-contents (expand-file-name ,file (eldev--test-project-dir ,test-project)) t))
      ,@body
      (let ((backup-inhibited t))
        (save-buffer))))

test/init.el
@@ -128,6 +128,7 @@
 
 (ert-deftest eldev-init-git-1 ()
   (eldev--test-with-temp-copy "project-a" 'Git
+    :ignores nil
     (eldev--test-delete-quietly nil "Eldev")
     (eldev--test-run nil ("init" "--non-interactive")
       (should (string= stdout (eldev-format-message "Created file `%s' for this project\nModified file `.gitignore'\n" eldev-file)))
@@ -139,6 +140,7 @@
 
 (ert-deftest eldev-init-hg-1 ()
   (eldev--test-with-temp-copy "project-a" 'Hg
+    :ignores nil
     (eldev--test-delete-quietly nil "Eldev")
     (eldev--test-run nil ("init" "--non-interactive")
       (should (string= stdout (eldev-format-message "Created file `%s' for this project\nModified file `.hgignore'\n" eldev-file)))
@@ -147,6 +149,18 @@
 ^\\.eldev$
 ^Eldev-local$
 "))))))
+
+(ert-deftest eldev-init-svn-1 ()
+  (eldev--test-with-temp-copy "project-a" 'SVN
+    :ignores nil
+    (eldev--test-delete-quietly nil "Eldev")
+    (eldev--test-run nil ("init" "--non-interactive")
+      (should (string= stdout (eldev-format-message "Created file `%s' for this project\nModified property `svn:ignore'\n" eldev-file)))
+      (eldev-call-process (eldev-svn-executable) '("propget" "svn:ignore" ".")
+        (should (string= (string-trim (buffer-string)) (eldev-format-message "\
+.eldev
+Eldev-local")))
+        (should (= exit-code 0))))))
 
 
 (provide 'test/init)

commit 9ca9abb3edca233ab52ada40531bec4ce3a8e642

Fix byte-compilation warning caused by commit d4a116c.

eldev.el
@@ -77,6 +77,15 @@
 (defvar package-archive-priorities)
 (defvar byte-compile-log-warning-function)
 
+;; Using `autoload' function directly instead of ;;;###autoload cookies since the latter
+;; won't work with ELDEV_LOCAL.  This must be at the top _and_ in `eval-and-compile', else
+;; byte-compilation gives warnings...
+(eval-and-compile
+  (dolist (autoloads '(("eldev-plugins" eldev-active-plugins eldev-use-plugin)
+                       ("eldev-vc"      eldev-vc-root-dir eldev-vc-executable eldev-vc-full-name eldev-with-vc eldev-vc-detect)))
+    (dolist (function (cdr autoloads))
+      (autoload function (car autoloads)))))
+
 
 (defvar eldev-shell-command (or (eldev-getenv "ELDEV_CMD") "eldev")
   "Command used to launch Eldev, in raw form.
@@ -4914,14 +4923,6 @@ will fail if the project already has file named `Eldev'."
   ("Don't ask any questions"
    :options       (-n --non-interactive))
   :for-command    init)
-
-
-;; Using `autoload' function directly instead of ;;;###autoload cookies since the latter
-;; won't work with ELDEV_LOCAL.
-(dolist (autoloads '(("eldev-plugins" eldev-active-plugins eldev-use-plugin)
-                     ("eldev-vc"      eldev-vc-root-dir eldev-vc-executable eldev-vc-full-name eldev-with-vc eldev-vc-detect)))
-  (dolist (function (cdr autoloads))
-    (autoload function (car autoloads))))
 
 
 (provide 'eldev)