95fedcc075afba7306341e55122b8aa6108d412e
Paul Pogonyshev <pogonyshev@gmail.com>
2021-10-19 21:18:36 +0200

    Release Eldev 0.9.3 because of a workaround important for certain projects using Org, at least.

--- eldev.el
+++ eldev.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    0.9.3snapshot
+;; Version:    0.9.3
 ;; Keywords:   maint, tools
 ;; Homepage:   https://github.com/doublep/eldev
 ;; Package-Requires: ((emacs "24.4"))
