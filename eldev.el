f7cfdb8648624917afec0a687c6fa92ce8d7958f
Paul Pogonyshev <pogonyshev@gmail.com>
2022-02-21 21:53:52 +0100

    Release Eldev 0.11 with profiling, several evaluation improvements and support for XDG-recommended layout.

--- eldev.el
+++ eldev.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    0.10.4snapshot
+;; Version:    0.11
 ;; Keywords:   maint, tools
 ;; Homepage:   https://github.com/doublep/eldev
 ;; Package-Requires: ((emacs "24.4"))
