b5fc7178aa56513454ff2b3dd8e1b2b4e272d93a
Paul Pogonyshev <pogonyshev@gmail.com>
2023-06-02 21:06:32 +0200

    Further corrupt package headers in `uninitialized-e.el', since in snapshot Emacs missing footer is no longer an error, only a warning.

--- test/uninitialized-e/uninitialized-e.el
+++ test/uninitialized-e/uninitialized-e.el
@@ -2,6 +2,6 @@
 
 ;; Version: 1.0
 ;; Homepage: https://example.com/
-;; Package-Requires: (dependency-a dependency-b dependency-c)
+;; Package-Requires: (dependency-a dependency-b dependency-c
 
 (provide 'uninitialized-e)
