2307625a4953f3706608f0dae7c3e9dc3444f8bb
Paul Pogonyshev <pogonyshev@gmail.com>
2023-06-06 20:09:42 +0200

    Release Eldev 1.4.1

--- eldev.el
+++ eldev.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    1.4.1snapshot
+;; Version:    1.4.1
 ;; Keywords:   maint, tools
 ;; Homepage:   https://github.com/doublep/eldev
 ;; Package-Requires: ((emacs "24.4"))
