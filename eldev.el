220f54cbd2398843c91065bc3a2375fe8934dcdf
Paul Pogonyshev <pogonyshev@gmail.com>
2024-01-23 22:27:31 +0100

    Release Eldev 1.8.2

--- eldev.el
+++ eldev.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    1.8.2snapshot
+;; Version:    1.8.2
 ;; Keywords:   maint, tools
 ;; Homepage:   https://github.com/emacs-eldev/eldev
 ;; Package-Requires: ((emacs "24.4"))
