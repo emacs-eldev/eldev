cb49c855ff42ffc04d7b1a7dcc2ababc4b8521a7
Paul Pogonyshev <pogonyshev@gmail.com>
2026-06-11 22:48:24 +0200

    Update copyright notices to include 2026.

--- eldev-buttercup.el
+++ eldev-buttercup.el
@@ -1,6 +1,6 @@
 ;;; eldev-buttercup.el --- Elisp development tool  -*- lexical-binding: t -*-
 
-;;; Copyright (C) 2020-2025 Paul Pogonyshev
+;;; Copyright (C) 2020-2026 Paul Pogonyshev
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
